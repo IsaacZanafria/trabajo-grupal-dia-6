001ba7b4ea6b434d5ea316a04b9fd85d4f2221f5
isaaczanafria <isaaczanafria@gmail.com>
2025-04-09 14:32:28 -0500

    funcionamiento del imageDiv

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { Link, Route, Routes } from "react-router";
+import Home from "./pages/Home";
 export default function App() {
   return (
     <>
+      <nav className='flex justify-end items-center h-16 bg-cyan-800 shadow-md'>
+        <ul className='flex gap-4 px-4 font-semibold text-white'>
+          <li><Link to="/">Home</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path='/:id?' element={<Home />} />
+      </Routes>
+
     </>
   )
 }
